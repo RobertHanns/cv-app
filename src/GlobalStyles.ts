825322634ce15ea92ed2af6670584f175997d394
Robert <robert_hanns@hotmail.com>
2023-03-20 22:05:40 +0000

    feat: mobile resize

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -7,6 +7,8 @@ body {
     padding: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     background-image: url(${Image});
+    background-repeat: no-repeat;
+  background-color: rgba(31, 41, 55, 1.5);
     background-size: cover;
     background-position: center;
     height: 100vh;
